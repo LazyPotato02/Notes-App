5ff0e31fef39c058cc99f75e38ce44c26926666e
LazyPotato02 <74728512+LazyPotato02@users.noreply.github.com>
2023-08-27 17:40:01 +0300

    Back-end rework

--- back_end/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from authentication import views
-
-urlpatterns = [
-    path('home/', views.HomeView.as_view(), name='home'),
-    path('logout/', views.LogoutView.as_view(), name='logout')
-]
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Nav from './components/Nav'
-import App from './App'
-import './components/axios';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
 
