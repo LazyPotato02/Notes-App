5214de0ce46545bf2a8b35f5e4942d4103bcb1b2
LazyPotato02 <74728512+LazyPotato02@users.noreply.github.com>
2023-08-27 18:54:44 +0300

    Implemented authentication from react to django

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './views/home';
+import MainWrapper from './layouts/MainWrapper';
+import Login from './views/login';
+import PrivateRoute from './layouts/PrivateRoute';
+import Logout from './views/logout';
+import Private from './views/private';
+import Register from './views/register';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <MainWrapper>
+                <Routes>
+                    <Route
+                        path="/private"
+                        element={
+                            <PrivateRoute>
+                                <Private />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/logout" element={<Logout />} />
+                </Routes>
+            </MainWrapper>
+        </BrowserRouter>
+    );
 }
 
 export default App;
