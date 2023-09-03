ca26bfccb4185f44d4e2ea5244e1fd7c94edee71
LazyPotato02 <74728512+LazyPotato02@users.noreply.github.com>
2023-09-03 13:41:09 +0300

    Added create method to the front-end and created style for it

--- front_end/src/views/home.js
+++ front_end/src/views/home.js
@@ -4,6 +4,8 @@ import {useAuthStore} from "../store/auth";
 import Cookies from "js-cookie";
 import './home.css';
 
+import Modal from "react-modal";
+
 const Home = () => {
     const [fetchedData, setFetchedData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -12,7 +14,11 @@ const Home = () => {
         state.isLoggedIn,
         state.user,
     ]);
-
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [formData, setFormData] = useState({
+        title: "",
+        content: "",
+    });
     const [editingIndex, setEditingIndex] = useState(-1);
     const [editedItem, setEditedItem] = useState({title: "", content: "", is_done: false});
 
@@ -104,13 +110,13 @@ const Home = () => {
     };
 
 
-    const handleDeleteClick = async(index) => {
-         const headers = {
+    const handleDeleteClick = async (index) => {
+        const headers = {
             'Authorization': `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
         };
-         let i = index + 1
-         try {
+        let i = index + 1
+        try {
             const response = await fetch(`${apiUrl}${i}`, {
                 method: 'DELETE',
                 headers: headers,
@@ -119,19 +125,121 @@ const Home = () => {
             if (!response.ok) {
                 throw new Error('Delete failed');
             }
-            if (response.ok){
+            if (response.ok) {
                 window.location.reload(false)
             }
             // Update the local state with the edited item
         } catch (error) {
-            console.error('Error updating content:', error);
+            console.error('Error deleting content:', error);
         }
     }
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleCreateClick = () => {
+        const title = formData.title;
+        const content = formData.content;
+
+        if (!title || !content) {
+            alert("Please fill in both fields");
+            return;
+        }
+
+        // Assuming your API endpoint URL
+        const apiUrl = "http://localhost:8000/api-notes/notes";
+
+        // Create a request body with the form data
+        const requestBody = JSON.stringify({
+            title: title,
+            content: content,
+            creator_id: userId,
+            is_done: false
+        });
+
+        // Send a POST request to the API
+        fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${accessToken}`,
+
+            },
+            body: requestBody,
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                // Handle the API response here
+                console.log("API Response:", data);
+                // Optionally, you can perform additional actions after a successful API request
+            })
+            .catch((error) => {
+                // Handle errors here
+                console.error("API Error:", error);
+            });
+
+        // Reset the form data and close the modal
+        setFormData({
+            field1: "",
+            field2: "",
+        });
+        closeModal();
+        window.location.reload(false)
+    };
+
 
     return (
         <div>
             {isLoggedIn() ? <LoggedInView user={user()}/> : <LoggedOutView/>}
-
+            <div className={'create_note'}>
+                <button onClick={openModal}>Create</button>
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Create Modal"
+                    className='modalStyle'
+                >
+                    <div className={'wrapper'}>
+                        <h2>Create Item</h2>
+                        <input
+                            type="text"
+                            name="title"
+                            value={formData.title}
+                            placeholder="Title"
+                            onChange={handleInputChange}
+                        />
+                        <textarea
+                            type="text"
+                            name="content"
+                            value={formData.content}
+                            placeholder="Text"
+                            onChange={handleInputChange}
+                        />
+                        <div className={'buttons'}>
+                            <button onClick={handleCreateClick}>Submit</button>
+                            <button onClick={closeModal}>Cancel</button>
+                        </div>
+                    </div>
+                </Modal>
+            </div>
             <section className={'car__article'}>
                 {fetchedData.map((item, index) => (
                     <article key={item.id} className={`car__item`}>
@@ -162,7 +270,8 @@ const Home = () => {
                                 <p>Text: {item.content}</p>
                                 <p>Done: {isDone(String(item.is_done))}</p>
                                 <button className={'edit__btn'} onClick={() => handleEditClick(index)}>Edit</button>
-                                <button className={'delete__btn'} onClick={() => handleDeleteClick(index)}>Delete</button>
+                                <button className={'delete__btn'} onClick={() => handleDeleteClick(index)}>Delete
+                                </button>
 
                             </div>
                         )}
